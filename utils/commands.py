09cd89bf8dcf421cbe352f7174bc362f4eb34aa2
mistaketz <mistaketz@gmail.com>
2024-09-10 23:10:24 +0300

    New users added to table

--- utils/commands.py
+++ utils/commands.py
@@ -5,17 +5,22 @@ from aiogram.fsm.context import FSMContext
 from aiogram.utils.markdown import hlink
 from loader import dp, bot, sender
 from os import path
+from datetime import datetime
 
 from config import get_env, get_config
 import utils.kb as kb
 from states import UserState
+from database.model import DB
 
 
 # Команда старта бота
 @dp.message(CommandStart())
 async def command_start_handler(msg: Message, state: FSMContext) -> None:
     id = msg.from_user.id
-    print(id)
+    if not DB.select(id):
+        print("New user:", id)
+        DB.commit("insert into users (telegram_id, registered) values (?, ?)", [id, datetime.now()])
+
     await sender.send_message(id, "start")
     await state.set_state(UserState.default)
 
